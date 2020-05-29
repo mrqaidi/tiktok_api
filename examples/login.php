535478e3eb1921525e389d687bb3f6ca777e9a93
mgp25 <me@mgp25.com>
2020-05-29 02:35:58 +0200

    Example: Update login

--- examples/login.php
+++ examples/login.php
@@ -7,53 +7,58 @@ $debug = true;
 $authKey = 'YOUR_AUTH_KEY';
 ///////////////////////////
 
-///// USER ///////////////
-$username = '';
+///// ACCOUNT ///////////////
+$username = '';            // This can be either a username or an email.
 $password = '';
-$email = '';
 //////////////////////////
 
 ///// DEVICE INFO ///////////////
-$deviceInfo = [
-        'device_id' => '',
-        'openudid'  => '',
-        'iid'       => '',
-];
+// USE ONLY IF YOU WANT TO SPECIFY ONE
+$deviceInfo = null;
 /////////////////////////////////
 
 $tiktok = new \TikTokAPI\TikTok($debug);
 $tiktok->setAuthKey($authKey);
 
+$loginResponse = null;
 try {
-    $loginResponse = $tiktok->login($email, $username, $password, $deviceInfo);
-} catch(\TikTokAPI\Exception\AuthkeyException $e) {
+    $loginResponse = $tiktok->login($username, $password, $deviceInfo);
+} catch (\TikTokAPI\Exception\AuthkeyException $e) {
     echo 'Invalid or expired auth key.';
     exit();
-}
+} finally {
+    if ($loginResponse !== null) {
+        if (!$loginResponse->isOk()) {
+            if ($loginResponse->getData()->getErrorCode() === 1105) {
+                $captchaData = $tiktok->getCaptcha($loginResponse->getData()->getErrorCode());
+                $captchaSolution = $tiktok->solveCaptcha($captchaData->getData()->getId(),
+                                      $captchaData->getData()->getQuestion()->getUrl1(),
+                                      $captchaData->getData()->getQuestion()->getUrl2(),
+                                      $captchaData->getData()->getQuestion()->getTipY());
+                sleep(3);
+                $tiktok->verifyCaptcha($captchaSolution);
+            } elseif ($loginResponse->getData()->getErrorCode() === 1009) {
+                echo $loginResponse->getData()->getDescription();
+                exit();
+            } elseif ($loginResponse->getData()->getErrorCode() === 7) {
+                echo "The account is in a cooldown.\n";
+                echo $loginResponse->getData()->getDescription();
+                exit();
+            }
+        }
+        $loginResponse = $tiktok->login($username, $password, $deviceInfo);
 
-if (!$loginResponse->isOk()) {
-    if ($loginResponse->getData()->getErrorCode() === 1105) {
-        $captchaData = $tiktok->getCaptcha($loginResponse->getData()->getErrorCode());
-        $captchaSolution = $tiktok->solveCaptcha($captchaData->getData()->getId(),
-                              $captchaData->getData()->getQuestion()->getUrl1(),
-                              $captchaData->getData()->getQuestion()->getUrl2(),
-                              $captchaData->getData()->getQuestion()->getTipY());
-
-        sleep(3);
-        $tiktok->verifyCaptcha($captchaSolution);
-    } elseif ($loginResponse->getData()->getErrorCode() === 1009) {
-        echo $loginResponse->getData()->getDescription();
-        exit();
+        if (!$loginResponse->isOk()) {
+            if ($loginResponse->getData()->getErrorCode() === 1009 || $loginResponse->getData()->getErrorCode() === 7) {
+                echo $loginResponse->getData()->getDescription();
+                exit();
+            }
+        }
     }
 }
 
-$loginResponse = $tiktok->login($email, $username, $password, $deviceInfo);
-
-if (!$loginResponse->isOk()) {
-    if ($loginResponse->getData()->getErrorCode() === 1009 || $loginResponse->getData()->getErrorCode() === 7) {
-        echo $loginResponse->getData()->getDescription();
-        exit();
-    }
+try {
+    $tiktok->getActivity();
+} catch (Exception $e) {
+    echo sprintf('Oops, something went wrong: %s', $e->getMessage());
 }
-
-$tiktok->getActivity();
